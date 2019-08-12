e56fa6b5940fac89eadff187d50a00e865ecd489
Mark McLoughlin <markmc@redhat.com>
2019-08-12 10:24:18 +0100

    Add network cleanup

--- host_cleanup.sh
+++ host_cleanup.sh
@@ -3,3 +3,21 @@ set -x
 
 source logging.sh
 source common.sh
+
+if virsh net-uuid baremetal > /dev/null 2>&1 ; then
+    virsh net-destroy baremetal
+    virsh net-undefine baremetal
+fi
+
+sudo ifdown baremetal || true
+sudo ip link delete baremetal || true
+sudo rm -f /etc/sysconfig/network-scripts/ifcfg-baremetal
+
+if virsh net-uuid provisioning > /dev/null 2>&1 ; then
+    virsh net-destroy provisioning
+    virsh net-undefine provisioning
+fi
+
+sudo ifdown provisioning || true
+sudo ip link delete provisioning || true
+sudo rm -f /etc/sysconfig/network-scripts/ifcfg-provisioning
