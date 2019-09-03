281bc54955550365a8fe3850ca9f24c6dc257bbc
Eduardo Minguez Perez <e.minguez@gmail.com>
2019-09-03 12:55:36 +0200

    Avoid removing the baremetal interface
    Fixes #52

--- OpenShift/host_cleanup.sh
+++ OpenShift/host_cleanup.sh
@@ -9,10 +9,6 @@ if virsh net-uuid baremetal > /dev/null 2>&1 ; then
     virsh net-undefine baremetal
 fi
 
-sudo ifdown baremetal || true
-sudo ip link delete baremetal || true
-sudo rm -f /etc/sysconfig/network-scripts/ifcfg-baremetal
-
 if virsh net-uuid provisioning > /dev/null 2>&1 ; then
     virsh net-destroy provisioning
     virsh net-undefine provisioning
