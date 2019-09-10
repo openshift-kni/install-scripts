02fc72bbd793eac07a07cfaf6d8d703eb770c627
Steve Reichard <sreichar@redhat.com>
2019-09-10 17:44:30 -0400

    Add timeout to input

--- common/ask.sh
+++ common/ask.sh
@@ -2,11 +2,18 @@
 
 PROMPT=$1
 
-read -p "${PROMPT} [ 'y' to continue ]: " -n 1 -r
+read -p "${PROMPT} [ 'y' to continue ]: " -n 1 -r -t 30
+ret=$?
 echo
-if [[ ${REPLY} =~ ^[Yy] ]]
+if [[ ${ret} -eq 0 ]]
 then
+   if [[ ${REPLY} =~ ^[Yy] ]]
+   then
 	exit 0
-else
+   else
 	exit -1
+  fi
+else [[ ${ret} -gt 128 ]]
+  # input timed out
+  exit 0
 fi
