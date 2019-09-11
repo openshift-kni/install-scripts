3a44533e192255935caf37feb08d4c15628d30c4
Steve Reichard <sreichar@redhat.com>
2019-09-11 08:29:17 -0400

    Changes from most reviews on 9/10 overnight

--- OpenShift/cache_images.sh
+++ OpenShift/cache_images.sh
@@ -8,12 +8,12 @@ source common.sh
 # Either pull or build the ironic images
 # To build the IRONIC image set
 # IRONIC_IMAGE=https://github.com/metalkube/metalkube-ironic
-for IMAGE_VAR in IRONIC_IMAGE IRONIC_INSPECTOR_IMAGE COREOS_DOWNLOADER_IMAGE ; do
+for IMAGE_VAR in IRONIC_IMAGE COREOS_DOWNLOADER_IMAGE ; do
     IMAGE=${!IMAGE_VAR}
     sudo podman pull "$IMAGE"
 done
 
-for name in ironic ironic-api ironic-conductor ironic-inspector dnsmasq httpd mariadb coreos-downloader; do
+for name in httpd coreos-downloader; do
     sudo podman ps | grep -w "$name$" && sudo podman kill $name
     sudo podman ps --all | grep -w "$name$" && sudo podman rm $name -f
 done
@@ -29,14 +29,14 @@ sudo podman pod create -n ironic-pod
 # We start only the httpd and *downloader containers so that we can provide
 # cached images to the bootstrap VM
 sudo podman run -d --net host --privileged --name httpd --pod ironic-pod \
-     -v $IRONIC_DATA_DIR:/shared --entrypoint /bin/runhttpd ${IRONIC_IMAGE}
+     -v ${CACHED_IMAGE_DIR}:/shared --entrypoint /bin/runhttpd ${IRONIC_IMAGE}
 
 sudo podman run -d --net host --privileged --name coreos-downloader --pod ironic-pod \
-     -v $IRONIC_DATA_DIR:/shared ${COREOS_DOWNLOADER_IMAGE} /usr/local/bin/get-resource.sh $RHCOS_IMAGE_URL
+     -v ${CACHED_IMAGE_DIR}:/shared ${COREOS_DOWNLOADER_IMAGE} /usr/local/bin/get-resource.sh ${RHCOS_IMAGE_URL}
 
 # Wait for the downloader containers to finish, if they are updating an existing cache
 # the checks below will pass because old data exists
 sudo podman wait -i 1000 coreos-downloader
 
 # Wait for images to be downloaded/ready
-while ! curl --fail http://localhost/images/rhcos-ootpa-latest.qcow2.md5sum ; do sleep 1 ; done
+while ! curl --fail http://localhost/images/rhcos-ootpa-latest.qcow2.md5sum ; do sleep 5 ; done
--- OpenShift/common.sh
+++ OpenShift/common.sh
@@ -19,7 +19,7 @@ source $CONFIG
 
 # Connect to system libvirt
 export LIBVIRT_DEFAULT_URI=qemu:///system
-if [ "$USER" != "root" -a "${XDG_RUNTIME_DIR:-}" == "/run/user/0" ] ; then
+if [ "${USER}" != "root" -a "${XDG_RUNTIME_DIR:-}" == "/run/user/0" ] ; then
     echo "Please use a non-root user, WITH a login shell (e.g. su - USER)"
     exit 1
 fi
@@ -45,29 +45,17 @@ if [ ${#PULL_SECRET} = 0 ]; then
 fi
 
 
-WORKING_DIR=${WORKING_DIR:-"/opt/install-scripts"}
 # Ironic vars
 export IRONIC_IMAGE=${IRONIC_IMAGE:-"quay.io/metal3-io/ironic:master"}
-export IRONIC_INSPECTOR_IMAGE=${IRONIC_INSPECTOR_IMAGE:-"quay.io/metal3-io/ironic-inspector:master"}
-export IPA_DOWNLOADER_IMAGE=${IPA_DOWNLOADER_IMAGE:-"quay.io/metal3-io/ironic-ipa-downloader:master"}
 export COREOS_DOWNLOADER_IMAGE=${COREOS_DOWNLOADER_IMAGE:-"quay.io/openshift-metal3/rhcos-downloader:master"}
-export IRONIC_DATA_DIR="$WORKING_DIR/ironic"
+export CACHED_IMAGE_DIR="${SCRIPTDIR}/cache"
 
 export KUBECONFIG="${SCRIPTDIR}/ocp/auth/kubeconfig"
 
-# Use a cloudy ssh that doesn't do Host Key checking
-export SSH="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=5"
 
-if [ ! -d "$WORKING_DIR" ]; then
-  echo "Creating Working Dir"
-  sudo mkdir "$WORKING_DIR"
-  sudo chown "${USER}:${USER}" "$WORKING_DIR"
-  chmod 755 "$WORKING_DIR"
-fi
-
-if [ ! -d "$IRONIC_DATA_DIR" ]; then
-  echo "Creating Ironic Data Dir"
-  sudo mkdir "$IRONIC_DATA_DIR"
-  sudo chown "${USER}:${USER}" "$IRONIC_DATA_DIR"
-  chmod 755 "$IRONIC_DATA_DIR"
+if [ ! -d "${CACHED_IMAGE_DIR}" ]; then
+  echo "Creating Cached Data Dir"
+  sudo mkdir "${CACHED_IMAGE_DIR}"
+  sudo chown "${USER}:${USER}" "${CACHED_IMAGE_DIR}"
+  chmod 755 "${CACHED_IMAGE_DIR}"
 fi
