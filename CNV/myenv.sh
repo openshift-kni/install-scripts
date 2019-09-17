caeb240be90983ad8023bcdc1c820bd350ae2017
redmikhail <mmikhail@redhat.com>
2019-09-17 19:24:58 -0400

    Added sample of myenv.sh file for olm-install.ch

---
+++ CNV/myenv.sh
@@ -0,0 +1,7 @@
+## Use this file to override values of default environment variables for olm-install.sh script 
+## Note: This file is meant for setting variables only, please avoid adding commands and logic to it
+##
+## Examples:
+#OPERATOR_NAME="hco-operatorhub"
+#CHANNEL_VERSION="2.2.0"
+#SUBSCRIPTION_APPROVAL="Automatic"
