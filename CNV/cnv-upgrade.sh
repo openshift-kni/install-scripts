5faa706e02af9f8028ab3ea03e517f8b72110103
karmab <karimboumedhel@gmail.com>
2019-09-20 18:12:40 +0200

    use a cnv version which allows upgrades

--- CNV/cnv-upgrade.sh
+++ CNV/cnv-upgrade.sh
@@ -2,20 +2,14 @@
 
 set -ex
 
-OLD_CNV_VERSION="${OLD_CNV_VERSION:-2.0.0}"
-CNV_VERSION="${CNV_VERSION:-2.1.0}"
-OLD_CNV_CHANNEL="${OLD_CNV_VERSION:0:3}"
-CNV_CHANNEL="${CNV_VERSION:0:3}"
 TARGET_NAMESPACE="${TARGET_NAMESPACE:-openshift-cnv}"
 
-oc get sub hco-subscription -o yaml -n "${TARGET_NAMESPACE}" | sed "s/channel: ${OLD_CNV_CHANNEL}/channel: ${CNV_CHANNEL}/" | oc apply -n "${TARGET_NAMESPACE}" -f -
+oc get installplan -o yaml -n ${TARGET_NAMESPACE} $(oc get installplan -n ${TARGET_NAMESPACE} --no-headers | grep kubevirt-hyperconverged-operator.v2.1.0 | awk '{print $1}') | sed 's/approved: false/approved: true/' | oc apply -n openshift-cnv -f -
 
-oc get installplan -o yaml -n "${TARGET_NAMESPACE}" $(oc get installplan -n "${TARGET_NAMESPACE}" --no-headers | grep kubevirt-hyperconverged-operator.v"${CNV_VERSION}" | awk '{print $1}') | sed 's/approved: false/approved: true/' | oc apply -n "${TARGET_NAMESPACE}" -f -
-
-echo "Waiting until OLM replaces the ${OLD_CNV_VERSION} CSV"
-echo "This could take up to 10 minutes..."
-while [ -z "$(oc get csv -o'custom-columns=status:status.conditions[-1].phase,metadata:metadata.name' --no-headers | grep kubevirt-hyperconverged-operator.v${CNV_VERSION} | grep Succeeded)" ]; do
-    echo "Waiting for ${CNV_VERSION} CSV to be in 'Succeeded'..."
-    oc get csv -o'custom-columns=status:status.conditions[-1].phase,metadata:metadata.name' --no-headers | grep kubevirt-hyperconverged-operator
+echo "Waiting until update finishes"
+echo "This could take up some time..."
+oc get hco -n ${TARGET_NAMESPACE} hyperconverged-cluster -o=jsonpath='{range .status.conditions[*]}{.type}{"\t"}{.status}{"\t"}{.message}{"\n"}{end}'
+while [ $(oc get hco -n ${TARGET_NAMESPACE} hyperconverged-cluster -o=jsonpath='{range .status.conditions[*]}{.type}{"\t"}{.status}{"\t"}{.message}{"\n"}{end}' | grep -c 'Reconcile completed successfully') != "5"] ; then do
+    echo "Waiting for HCO to upgrade ..."
     sleep 30
 done
